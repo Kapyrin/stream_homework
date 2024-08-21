bd371222cc05341d201fcc462d4e0ce8d5a43cb2
Vladimir Kapyrine <vladimir.kapyrin@gmail.com>
2024-08-21 12:00:16 +0100

    Initial commit

---
+++ src/main/java/kapyrin/PuttingIntoPractice.java
@@ -0,0 +1,58 @@
+package kapyrin;
+
+import kapyrin.entity.Trader;
+import kapyrin.entity.Transaction;
+import kapyrin.logic.StreamLogic;
+
+import java.util.*;
+
+public class PuttingIntoPractice {
+
+    public static void main(String... args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        StreamLogic logic = new StreamLogic();
+
+        //1. Найти все транзакции за 2011 год и отсортировать их по сумме (от меньшей к большей).
+        logic.allTransactionsFrom2021SortedBySum(transactions, 2011);
+
+
+        //2. Вывести список неповторяющихся городов, в которых работают трейдеры.
+        logic.listOfUniqueCitiesWhereTradersWorks(transactions);
+
+        //3. Найти всех трейдеров из Кембриджа и отсортировать их по именам.
+        List<Trader> allTradersFromCambridgeSorted = logic.findAllTradersFromCambridgeSorted(transactions);
+        System.out.println(allTradersFromCambridgeSorted);
+
+
+        //4. Вернуть строку со всеми именами трейдеров, отсортированными в алфавитном порядке.
+        String sortedByNames = logic.sortedTradersNames(transactions);
+
+        //5.Выяснить, существует ли хоть один трейдер из Милана.
+       logic.existOrNotTraderFromCity(transactions, "Milan");
+
+        //6. Вывести суммы всех транзакций трейдеров из Кембриджа.
+        logic.sumOfAllTradersTransactions(transactions);
+
+        //7. Какова максимальная сумма среди всех транзакций?
+       logic.maximumTransaction(transactions);
+
+
+        //8. Найти транзакцию с минимальной суммой.
+        logic.minimumTransaction(transactions);
+    }
+
+
+}
---
+++ src/main/java/kapyrin/logic/StreamLogic.java
@@ -0,0 +1,76 @@
+package kapyrin.logic;
+
+import kapyrin.entity.Trader;
+import kapyrin.entity.Transaction;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+
+public class StreamLogic {
+
+    public void allTransactionsFrom2021SortedBySum(List<Transaction> transactions, int year) {
+        List<Transaction> trForYear = transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+        System.out.println(trForYear);
+    }
+
+    public void listOfUniqueCitiesWhereTradersWorks(List<Transaction> transactions) {
+        List<String> uniqueCities = transactions.stream()
+                .map(t -> t.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(uniqueCities);
+    }
+
+    public List<Trader> findAllTradersFromCambridgeSorted(List<Transaction> transactions) {
+        return transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(t -> t.getCity().contains("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public String sortedTradersNames(List<Transaction> transactions) {
+        String sortedTradersName = transactions.stream()
+                .map(t -> t.getTrader().getName())
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining("\n"));
+        return sortedTradersName;
+    }
+
+    public void existOrNotTraderFromCity(List<Transaction> transactions, String city) {
+        String existTraderFromMilan = transactions.stream()
+                .anyMatch(t -> t.getTrader().getCity().contains(city)) ? "yes" : "no";
+        System.out.println(existTraderFromMilan);
+    }
+
+    public void sumOfAllTradersTransactions(List<Transaction> transactions) {
+        int sumOfAllTransactions = transactions.stream()
+                .filter(t -> t.getTrader().getCity().contains("Cambridge"))
+                .mapToInt(Transaction::getValue)
+                .sum();
+        System.out.println(sumOfAllTransactions);
+    }
+
+    public void maximumTransaction(List<Transaction> transactions) {
+        OptionalInt maxSumFromAllTransactions = transactions.stream()
+                .mapToInt(Transaction::getValue)
+                .max();
+        System.out.println(maxSumFromAllTransactions.getAsInt());
+    }
+
+    public void minimumTransaction(List<Transaction> transactions) {
+        OptionalInt minimumSum = transactions.stream()
+                .mapToInt(Transaction::getValue)
+                .min();
+        System.out.println(minimumSum.getAsInt());
+    }
+
+
+}
